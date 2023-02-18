e053078db3b0d89e28f0c2268451153122b8e248
Your Name <adamcrodgers@gmail.com>
2023-02-18 14:48:26 -0600

    reset button

--- connect.js
+++ connect.js
@@ -7,46 +7,78 @@ let player2Display = document.getElementById('player2Display')
 let pieces = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 let player1Name = document.getElementById('player1Name')
 let player2Name = document.getElementById('player2Name')
-let submit1 = document.getElementById('submit1')
-let submit2 = document.getElementById('submit2')
+const submit1 = document.getElementById('submit1')
 let player1Color = document.getElementById('player1Color')
+ color1=player1Color.value
 let player2Color = document.getElementById('player2Color')
+ color2=player2Color.value
+const submit2 = document.getElementById('submit2')
+// const submitColor1 = document.getElementById('submitColor1')
+// const submitColor2 = document.getElementById('submitColor2')
+const reset = document.getElementById('reset')
+
+PLAYER1 = color1
+PLAYER2 = color2
+playerTurn = PLAYER1
+
+function changeColor1(selectedObject){
+
+    playerTurn=selectedObject.value
+    console.log(selectedObject.value)
 
 
+}
 
-addEventListener("onclick", onclickSubmit1())
-addEventListener("onclick", onclickSubmit2())
+function changeColor2(selectedobject){
+    playerTurn=selectedObject.value
+    console.log(selectedObject.value)
+}
 
-
-
+//addEventListener("onclick", onclickSubmit1())
 function onclickSubmit1() {
-    PLAYER1 = 'RED'
+
     Name1 = player1Name.value
     player1Display.innerHTML = Name1
-    console.log(Name1)
 
     if (Name1) {
         submit1.style.display = 'none'
     }
-
+    console.log(Name1)
 }
+
+// addEventListener("onclick", onclickSubmit2())
 function onclickSubmit2() {
-    PLAYER2 = 'YELLOW'
+
     Name2 = player2Name.value
     player2Display.innerHTML = Name2
-    console.log(Name2)
 
     if (Name2) {
         submit2.style.display = 'none'
     }
-
+    console.log(Name2)
 }
 
 
-playerTurn = PLAYER1
+// addEventListener("onclick", onclickSubmitColor1())
+// function onclickSubmitColor1() {
 
+//     let color1 = player1Color.value
+//     if (color1) {
+//         submitColor1.style.display = 'none'
+//     }
+//     console.log(color1)
 
+// }
 
+// addEventListener("onclick", onclickSubmitColor2())
+// function onclickSubmitColor2() {
+//     let color2 = player1Color.value
+//     if (color2) {
+//         submitColor2.style.display = 'none'
+//     }
+//     console.log(color2)
+
+// }
 
 for (let i = 0; i < 42; i++) {
     let cell = document.createElement("div");
@@ -58,13 +90,10 @@ for (let i = 0; i < 42; i++) {
     cell.onclick = () => {
         onclickColumn(i % 7)
     }
-
-
 }
 
 
 function onclickColumn(column) {
-
     let availableSpace = pieces.filter((_, index) => index % 7 === column).lastIndexOf(0)
     pieces[(availableSpace * 7) + column] = playerTurn
     let cell = board.children[(availableSpace * 7) + column]
@@ -158,7 +187,6 @@ function checkForWinner() {
 
 
 function winner() {
-
     winModal = document.createElement('div')
     winModal.className = 'winModal'
     winModal.style = "text-transform: uppercase"
@@ -166,13 +194,10 @@ function winner() {
         winModal.style.color = 'black'
         winModal.style.backgroundColor = PLAYER1
         winModal.innerText = player1Display.innerHTML + " " + "WINS!!"
-
-
     } else if (playerTurn === PLAYER2) {
         winModal.style.color = 'black'
         winModal.style.backgroundColor = PLAYER2
         winModal.innerText = player2Display.innerHTML + " " + "WINS!!"
-
     }
     board.append(winModal)
     winModal.onclick = () => {
@@ -181,33 +206,16 @@ function winner() {
 
 }
 
-
-
+addEventListener("onclick", onclickReset())
 function onclickWinModal() {
-    //  winModal = document.getElementsByClassName('winModal')
-    // winModal.className = 'winModal'
-
-    pieces = pieces.map((m, i) => {
-        m = 0
-        let removeableChild = document.getElementById(i).firstChild
-        if (removeableChild) {
-            document.getElementById(i).removeChild(removeableChild)
-        }
-        return m
-
-    }
-    )
+    onclickReset()
     winModal.remove()
-
 
     if (playerTurn === PLAYER1) {
         let player2Score = p2Score.innerText
         let result = Number(player2Score)
         p2Score.innerText = result + 1
         result++
-        console.log(result)
-
-
 
     }
 
@@ -216,16 +224,34 @@ function onclickWinModal() {
         let result = Number(player1Score)
         p1Score.innerText = result + 1
         result++
-        console.log(result)
-
 
     }
 
-    playerTurn = PLAYER1
+    if (result === Number(player1Score)) {
+        playerTurn = PLAYER1
+    }
 
-
+    else if (result === Number(player2Score)) {
+        playerTurn = PLAYER2
+    }
 }
 
+
+addEventListener("onclick", onclickReset())
+function onclickReset() {
+    pieces = pieces.map((m, i) => {
+        m = 0
+        let removeableChild = document.getElementById(i).firstChild
+        if (removeableChild) {
+            document.getElementById(i).removeChild(removeableChild)
+        }
+        return m
+    }
+    )
+
+    document.body.style.backgroundColor = 'black'
+
+}
 
 function draw() {
     let drawModal = document.createElement('div')
@@ -235,9 +261,6 @@ function draw() {
     drawModal.onclick = () => {
         onclickDrawModal()
     }
-
-
-
 }
 
 
